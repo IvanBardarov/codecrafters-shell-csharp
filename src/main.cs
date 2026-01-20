bf9b715e561475df3355f02a89f38ec13af42198
Ivan Bardarov <ivanbardarov73@gmail.com>
2026-01-20 15:54:24 +0200

    handle invalid commands

--- src/main.cs
+++ src/main.cs
@@ -2,7 +2,11 @@ class Program
 {
     static void Main()
     {
-        // TODO: Uncomment the code below to pass the first stage
-         Console.Write("$ ");
+        Console.Write("$ ");
+
+        // Captures the user's command in the "command" variable
+        string command = Console.ReadLine();
+
+        Console.WriteLine($"{command}: command not found");
     }
 }
