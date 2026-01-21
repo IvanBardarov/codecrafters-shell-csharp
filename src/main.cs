c3d490a8b0269708dc239cccd52ea33572b9086f
Ivan Bardarov <ivanbardarov73@gmail.com>
2026-01-21 22:22:05 +0200

    codecrafters submit [skip ci]

--- src/main.cs
+++ src/main.cs
@@ -7,14 +7,28 @@ class Program
             Console.Write("$ ");
 
             // Captures the user's command in the "command" variable
-            string? command = Console.ReadLine();
+            string? userInput = Console.ReadLine();
 
+            string[]? userInputArray = userInput?.Split(' ');
+
+            string? command = userInputArray?[0];
+            string[]? args = userInputArray?[1..];
             if(command == "exit")
             {
                 break;
             }
-
-            Console.WriteLine($"{command}: command not found");
+            else if(command == "echo")
+            {
+                if(args != null)
+                {
+                    string output = string.Join(" ", args);
+                    Console.WriteLine(output);
+                }
+            }
+            else
+            {
+                Console.WriteLine($"{command}: command not found");
+            }            
         }
 
     }
